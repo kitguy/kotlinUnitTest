2d4364c00079217faeafa34e9e9ec92f0892dbb3
Haruki Serikawa <rhoserikawa@veltec.com.br>
2019-08-07 08:11:56 -0300

    Remove StateService

--- src/main/java/StateService.kt
+++
@@ -1,7 +0,0 @@
-import TestState.NumberEnum
-
-class StateService() {
-    fun getState(): NumberEnum {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
