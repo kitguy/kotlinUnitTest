995d32eb7fc9c6ebbe9ce4d575bd8ea1814bc43e
Haruki Serikawa <rhoserikawa@veltec.com.br>
2019-08-07 09:25:02 -0300

    Remove dependência do spring

--- src/main/java/TestRepository.kt
+++ src/main/java/TestRepository.kt
@@ -1,6 +1,3 @@
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Repository
-
 open class TestRepository {
 
     open fun getIntegerList(): Collection<Int> {
